dd5539d1c9b54fe5ebae0537cbae0869ab007d96
Quentin <qbouchard97@gmail.com>
2024-09-08 22:02:22 +0200

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <header>
+            <h1>&lt;/&gt;</h1>
+            <nav className="menu">
+                <ul>
+                    <a href="#" >Home</a>
+                    <a href="#" >Projects</a>
+                    <a href="#" >Skills</a>
+                    <a href="#" >Contact</a>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
