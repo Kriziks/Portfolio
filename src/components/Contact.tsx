c6dd31929abd4f4ea758a50f53f7b930a706b064
Quentin <qbouchard97@gmail.com>
2024-09-12 16:54:10 +0200

    Contact form submitting an email to me

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,28 @@
 import './Contact.css'
+import { useState } from "react";
 
 function Contact(){
+    const [status, setStatus] = useState("Submit")
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setStatus("Sending...")
+        const { name, email, message } = e.target.elements
+        let details = {
+            name: name.value,
+            email: email.value,
+            message: message.value
+        }
+        let response = await fetch("http://localhost:5000/contact", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json;charset=utf-8"
+            },
+            body: JSON.stringify(details)
+        });
+        setStatus("Submit")
+        let result = await response.json();
+        alert(result.status); 
+    }
     return(
         <section id="contact" className="contact">
             <div className="contact-root">
@@ -14,11 +36,11 @@ function Contact(){
                         </p>
                     </div>
                 </div>
-                <form className="contact-form">
-                <input type="text" placeholder="Name" className="contact-text" />
-                <input type="email" placeholder="Email" className="contact-email" />
-                <textarea placeholder="Message" className="contact-message" />
-                <button className="contact-button">Submit</button>
+                <form className="contact-form" onSubmit={handleSubmit}>
+                    <input type="text" id="name" placeholder="Name" className="contact-name" required/>
+                    <input type="email" id="email" placeholder="Email" className="contact-email" required/>
+                    <textarea id="message" placeholder="Message" className="contact-message" required />
+                    <button className="contact-button">{status}</button>
                 </form>
             </div>
         </section>
